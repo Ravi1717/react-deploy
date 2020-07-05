5c868369ce611aef543854a80de81592acb06a7c
ravi1717 <vermarv0802@gmail.com>
2020-07-05 20:29:57 +0530

    deploy my react app to github pages

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is a Create App</h1>
+      <h2>Author: Ravi Verma</h2>
+    </div>
+  );
+}
+
+export default App;
